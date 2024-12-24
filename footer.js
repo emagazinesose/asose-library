9ec5d8bcdb80dd9743990309e8c7f17f0b52f2e4
schoolop08 <soserohini11.9a.12.omprakashsahoo@gmail.com>
2024-12-24 20:28:02 +0530

    done

--- footer.js
+++ footer.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // Add content to the footer
   footer.innerHTML = `
       <p>&copy; 2024 ASOSE, Sector 11, Rohini</p>
-      <p>Developed & Maintained with &hearts; by <a href="/developer.html">Om Prakash Sahoo</a></p>
+      <p>Developed & Maintained with &hearts; by <a class="golden-title" style="font-size:18px" href="/developer.html">Om Sahoo</a></p>
     `;
 
   // Append the footer to the body
